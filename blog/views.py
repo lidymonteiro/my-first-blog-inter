27dea08b3695e27d08e45433a0e850102a859531
lidymonteiro <contato.lidymonteiro@hotmail.com>
2019-10-19 22:54:37 -0300

    created views blog

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def post_list(request):
-    return HttpResponse('<h1>Olá mundo!</h1>')
+    return render(request, 'blog/post_list.html', {})
